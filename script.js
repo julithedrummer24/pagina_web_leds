5dbb1000b6d98c503112247ed9a5a14a726fc7cf
julianandresyamartecastellanos@gmail.com <julianandresyamartecastellanos@gmial.com>
2024-10-19 22:03:07 -0500

    optimizacion

--- script.js
+++ script.js
@@ -1,21 +1,302 @@
+const proyectos =[
+    {
+      "imageUrl": "./assets/15_años.png",
+      "caracteristicas": "Todos nuestros letreros hechos para toda ocasión y negocio",
+      "medidas": "60 * 40 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/abierto.png",
+      "caracteristicas": "A la hora de iluminar tu negocio Siempre La Mejor Opción",
+      "medidas": "50 * 20 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/arbol.png",
+      "caracteristicas": "Iluminación de cualquier tipo de imagen decorativa",
+      "medidas": "40 * 60 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/bar1.png",
+      "caracteristicas": "Di lo que necesites con nuestros letreros hechos a medida",
+      "medidas": "3 paneles 60(max) * 30 cm",
+      "precio": "300.000 $"
+    },
+    {
+      "imageUrl": "./assets/ps.png",
+      "caracteristicas": "Para toda decoración Gamer",
+      "medidas": "50 * 30 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/bar2.jpg",
+      "caracteristicas": "Di lo que quieras con nuestros letreros para tu negocio",
+      "medidas": "60 * 80 cm",
+      "precio": "300.000 $"
+    },
+    {
+      "imageUrl": "./assets/bar3.jpg",
+      "caracteristicas": "Un lugar más iluminado es un sitio más agradable",
+      "medidas": "60 * 60 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/bomba.jpg",
+      "caracteristicas": "Decoración para tu hijo o hija hogar y más",
+      "medidas": "50 * 50 cm",
+      "precio": "150.000 $"
+    },
+    {
+      "imageUrl": "./assets/spiderman.jpg",
+      "caracteristicas": "Sorprende a tu hijo o hija con un bello letrero de superhéroe",
+      "medidas": "40 * 50 cm",
+      "precio": "250.000 $"
+    },
+    {
+      "imageUrl": "./assets/burguer.jpg",
+      "caracteristicas": "Cualquier letrero es indispensable para que tu negocio sea reconocido",
+      "medidas": "80 * 60 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/burguer1.jpg",
+      "caracteristicas": "Ilumina tu negocio y llama la atención de tus clientes",
+      "medidas": "80 * 60 cm",
+      "precio": "260.000 $"
+    },
+    {
+      "imageUrl": "./assets/cat.jpg",
+      "caracteristicas": "Adorna tu cuarto u hogar con la iluminación que tú desees",
+      "medidas": "60 * 50 cm",
+      "precio": "150.000 $"
+    },
+    {
+      "imageUrl": "./assets/escudo.jpg",
+      "caracteristicas": "Ambientamos cualquier negocio también para tus espacios de juego",
+      "medidas": "80 * 50 cm",
+      "precio": "300.000 $"
+    },
+    {
+      "imageUrl": "./assets/letra1.png",
+      "caracteristicas": "Ilumina tu marca, tu apodo, tu nickname",
+      "medidas": "60 * 50 cm",
+      "precio": "160.000 $"
+    },
+    {
+      "imageUrl": "./assets/fondo.png",
+      "caracteristicas": "Danos tus ideas y las hacemos realidad",
+      "medidas": "xx * xx cm",
+      "precio": "80.000 $"
+    },
+    {
+      "imageUrl": "./assets/letra3.png",
+      "caracteristicas": "Algo que ilumine y alegre tu cuarto",
+      "medidas": "60 * 20 cm",
+      "precio": "130.000 $"
+    },
+    {
+      "imageUrl": "./assets/goku.png",
+      "caracteristicas": "Regala a ese amor lo que más le gusta",
+      "medidas": "60 * 60 cm",
+      "precio": "250.000 $"
+    },
+    {
+      "imageUrl": "./assets/hall1.jpg",
+      "caracteristicas": "Decora tus épocas favoritas con estilo",
+      "medidas": "40 * 30 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/letra6.png",
+      "caracteristicas": "Sin miedo a que se dañe por el agua, aquí lo impermeabilizamos",
+      "medidas": "60 * 20 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/letra7.png",
+      "caracteristicas": "Deja que tu marca ilumine a tus clientes",
+      "medidas": "60 * 20 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/hongo.png",
+      "caracteristicas": "Otro pequeño regalo que puedes dar",
+      "medidas": "60 * 20 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/letra2.jpg",
+      "caracteristicas": "Que siempre brille tu nombre",
+      "medidas": "60 * 20 cm",
+      "precio": "140.000 $"
+    },
+    {
+      "imageUrl": "./assets/yoshi.jpg",
+      "caracteristicas": "Ilumina tus espacios",
+      "medidas": "60 * 20 cm",
+      "precio": "250.000 $"
+    },
+    {
+      "imageUrl": "./assets/link.jpg",
+      "caracteristicas": "Lo que sueñes, lo hacemos realidad",
+      "medidas": "60 * 20 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/pizza.png",
+      "caracteristicas": "Haz que tu negocio crezca y sea más visible",
+      "medidas": "80 * 100 cm",
+      "precio": "450.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo1.jpg",
+      "caracteristicas": "Danos tu logo, tu diseño y lo iluminamos",
+      "medidas": "80 * 50 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo2.jpg",
+      "caracteristicas": "Dilo con amor y luz",
+      "medidas": "60 * 20 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo3.jpg",
+      "caracteristicas": "Informa a tus clientes de todo en tu negocio",
+      "medidas": "50 * 30 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/mando.jpg",
+      "caracteristicas": "Llama a esos players a tu negocio",
+      "medidas": "50 * 50 cm",
+      "precio": "300.000 $"
+    },
+    {
+      "imageUrl": "./assets/mickey.png",
+      "caracteristicas": "No importa qué tan difícil parezca, siempre se puede hacer real",
+      "medidas": "100 * 50 cm",
+      "precio": "300.000 $"
+    },
+    {
+      "imageUrl": "./assets/navidad1.png",
+      "caracteristicas": "Para que festejes la Navidad bien iluminada",
+      "medidas": "50 * 40 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/pig.jpg",
+      "caracteristicas": "Todo lo que quieras iluminar siempre se puede",
+      "medidas": "60 * 40 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/pink.jpg",
+      "caracteristicas": "Claro que no falte aquella caricatura que marcó esa infancia",
+      "medidas": "60 * 30 cm",
+      "precio": "220.000 $"
+    },
+    {
+      "imageUrl": "./assets/piña.jpg",
+      "caracteristicas": "Claro que no falte aquella caricatura que marcó esa infancia",
+      "medidas": "30 * 40 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/hall2.jpg",
+      "caracteristicas": "Ilumina tus festividades favoritas",
+      "medidas": "70 * 50 cm",
+      "precio": "150.000 $"
+    },
+    {
+      "imageUrl": "./assets/fantasma.jpg",
+      "caracteristicas": "Zona Gamer Active",
+      "medidas": "50 * 50 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo4.jpg",
+      "caracteristicas": "Enmarca tu logo, tu nickname, tu frase favorita o palabra favorita",
+      "medidas": "60 * 30 cm",
+      "precio": "160.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo5.jpg",
+      "caracteristicas": "Zona Gamer Active",
+      "medidas": "80 * 80 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo6.jpg",
+      "caracteristicas": "Espanta en Noche de Brujas",
+      "medidas": "80 * 80 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo7.jpg",
+      "caracteristicas": "Lindos adornos luminosos para tu hogar",
+      "medidas": "40 * 60 cm",
+      "precio": "200.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo8.jpg",
+      "caracteristicas": "Tu idea, nuestro trabajo principal",
+      "medidas": "20 * 60 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo9.jpg",
+      "caracteristicas": "Disfruta decorando tu zona gamer con mucha elegancia",
+      "medidas": "50 * 20 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo10.jpg",
+      "caracteristicas": "Cualquier idea que desees siempre será puesta en marcha",
+      "medidas": "40 * 50 cm",
+      "precio": "120.000 $"
+    },
+    {
+      "imageUrl": "./assets/logo11.jpg",
+      "caracteristicas": "Sé la envidia en tu hogar iluminado",
+      "medidas": "40 * 40 cm",
+      "precio": "150.000 $"
+    },
+    {
+      "imageUrl": "./assets/pig2.jpg",
+      "caracteristicas": "Un bello detalle para el hogar o regalar",
+      "medidas": "40 * 60 cm",
+      "precio": "180.000 $"
+    },
+    {
+      "imageUrl": "./assets/control.jpg",
+      "caracteristicas": "Un buen detalle para tu zona gamer",
+      "medidas": "40 * 40 cm",
+      "precio": "150.000 $"
+    }
+  ]
+  
 
-// const contenidoCard = document.getElementById("proyectos");
+const contenidoCard = document.querySelector(".proyectos");
 
-// proyectos.forEach(proyecto => {
-//   const card = document.createElement("div");
-//       card.classList.add("card");
-//       card.style.backgroundImage = `url(${proyecto.imageUrl})`;
+proyectos.forEach(proyecto => {
+  const card = document.createElement("div");
+      card.classList.add("card");
+      card.style.backgroundImage = `url(${proyecto.imageUrl})`;
 
-//       const detalle = document.createElement("div");
-//       detalle.classList.add("detalle");
-//       detalle.innerHTML = `
-//               <p>${proyecto.caracteristicas}</p>
-//               <p>${proyecto.medidas}</p>
-//               <h1>${proyecto.precio}</h1>
-//       `
-//         card.appendChild(detalle);
-//         contenidoCard.appendChild(card);
-// });
+      const detalle = document.createElement("div");
+      detalle.classList.add("detalle");
+      detalle.innerHTML = `
+              <p>${proyecto.caracteristicas}</p>
+              <p>${proyecto.medidas}</p>
+              <h1>${proyecto.precio}</h1>
+      `
+        card.appendChild(detalle);
+        contenidoCard.appendChild(card);
+});
+
+
 
 // fetch("data.js")
 //   .then((res) => res.json())
